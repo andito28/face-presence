c6fbf654c32df52a6587f0062f231f95ec3967c7
andito28 <andito763@gmail.com>
2025-01-05 21:41:19 +0800

    add edit and update user

--- resources/js/Pages/User/Index.jsx
+++ resources/js/Pages/User/Index.jsx
@@ -30,20 +30,48 @@ export default function UserIndex({users}) {
                             <table className="min-w-full">
                                 <thead>
                                     <tr className="border-b-2">
-                                        <th className="px-6 py-3 text-left text-lg font-medium text-black">Id</th>
-                                        <th className="px-6 py-3 text-left text-lg font-medium text-black">Name</th>
-                                        <th className="px-6 py-3 text-left text-lg font-medium text-black">Email</th>
-                                        <th className="px-6 py-3 text-left text-lg font-medium text-black">Role</th>
+                                        <th className="px-6 py-3 text-left text-lg font-medium text-black">
+                                            Id
+                                        </th>
+                                        <th className="px-6 py-3 text-left text-lg font-medium text-black">
+                                            Name
+                                        </th>
+                                        <th className="px-6 py-3 text-left text-lg font-medium text-black">
+                                            Email
+                                        </th>
+                                        <th className="px-6 py-3 text-left text-lg font-medium text-black">
+                                            Role
+                                        </th>
+                                        <th className="px-6 py-3 text-left text-lg font-medium text-black">
+                                            &nbsp;
+                                        </th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {users.data.map((user) => {
                                         return (
                                             <tr key={user.id} className="border-b">
-                                                <td className="px-4 py-4 whitespace-nowrap">{user.id}</td>
-                                                <td className="px-4 py-4 whitespace-nowrap">{user.name}</td>
-                                                <td className="px-4 py-4 whitespace-nowrap">{user.email}</td>
-                                                <td className="px-4 py-4 whitespace-nowrap">{user.role}</td>
+                                                <td className="px-4 py-4 whitespace-nowrap">
+                                                    {user.id}
+                                                </td>
+                                                <td className="px-4 py-4 whitespace-nowrap">
+                                                    {user.name}
+                                                </td>
+                                                <td className="px-4 py-4 whitespace-nowrap">
+                                                    {user.email}
+                                                </td>
+                                                <td className="px-4 py-4 whitespace-nowrap">
+                                                    {user.role}
+                                                </td>
+                                                <td className="px-4 py-4 whitespace-nowrap">
+                                                    <Link
+                                                        className="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900"
+
+                                                        href={route("users.edit",user.id)}
+                                                    >
+                                                        Edit
+                                                    </Link>
+                                                </td>
                                             </tr>
                                         )
                                     })}
